9fe2a13004ba801b8f3f301e9dde53589c16af15
Henry Dang <henryd2002142@gmail.com>
2023-11-02 16:09:49 -0500

    Implement php functionality

--- Pages/Store/AddItemPage.php
+++ Pages/Store/AddItemPage.php
@@ -13,32 +13,10 @@
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-    <header class="navbar nav-background">
-        
-          <a class="navbar-brand logo" href="../Store/StorePage.html">Creative Ideas</a>
-
-            <form class="d-flex">
-                <input class="form-control me-sm-2" type="search" placeholder="Search" style="margin-left: 50px; width: 100%">
-                <button class="btn btn-secondary" type="button"><i class="bi bi-search"></i></button>
-            </form>
-
-            <div class="icons" style="font-size: 24px;">
-                <a href="https://github.com/hdang14/creativeIdeasFrontend/tree/Vincent's-Branch"><i style="color: black;" class="bi bi-github"></i></a>
-                <li class="nav-item dropdown" style="padding-left: 30px;">
-                    <a class="nav-link" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="bi bi-person"></i></a>
-                    <div class="dropdown-menu" style="background-color: #F5F5DC;">
-                      <h6 style="font-size: 17px;" class="dropdown-header">Hi, John Smith!</h6>
-                      <a style="font-size: 14px;" class="dropdown-item" href="#">View Order History</a>
-                      <a style="font-size: 14px;" class="dropdown-item" href="#">Logout</a>
-                    </div>
-                </li>
-                <a href="#"><i style="margin-left:25px; color: black;" class="bi bi-cart"></i></a>
-            </div>
-          </div>
-      </header>
+   <?php include("../OrderPages/Navbar.php") ?>
 
       <div class="center">
-        <form class="addform" action="process_item.php" method="POST">
+        <form class="addform"">
             <div class="form-group">
                 <label for="item_name">Name:</label>
                 <input type="text" id="item_name" name="item_name" required>
