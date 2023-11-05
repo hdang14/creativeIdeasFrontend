0c36fd6ed897dc8cbad49107fc3dab6ba2a8a052
Vincent Vannavong <vvanna00@g.uafs.edu>
2023-11-05 01:10:25 -0500

    Fixed php include

--- Pages/Store/AddItemPage.php
+++ Pages/Store/AddItemPage.php
@@ -13,7 +13,7 @@
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-   <?php include("../OrderPages/Navbar.php") ?>
+   <?php include("../Component/Navbar.php") ?>
 
       <div class="center">
         <form class="addform"">
